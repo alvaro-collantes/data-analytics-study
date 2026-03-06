09442f1212f8025655d87ebb33c38656aec03b9e
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-03-06 10:53:40 +0100

    First try

--- week-02-filters/practice.sql
+++ week-02-filters/practice.sql
@@ -41,7 +41,7 @@ CREATE TABLE claims (
 
 INSERT INTO claims VALUES
 (201, 1, '2024-03-01', 20000, 'Paid'),
-(202, 1, '2024-04-15', 35000, 'Pending'),
+(202, 2, '2024-04-15', 35000, 'Pending'),
 (203, 3, '2024-03-22', 60000, 'Paid');
 
 #Explore data
@@ -53,16 +53,51 @@ select *
 from credit_assessments;
 #claims
 select *
-from credit_assessments;
+from claims;
 
 # Exercise 1 — Basic aggregation
 #Find the total claim amount per client name. Only show clients who have at least one claim.
 
+select client_name, sum(claim_amount) as total_claim_amount
+from claims cla
+join clients cli
+	on cla.client_id = cli.client_id
+where cla.client_id >= 1
+group by client_name; 
+    
+#Join: merge both tables, all columns in the same table where they match
+#indicate each table with a prefix
+#where: goes after join
+#If you use an agg function, you need to group by
+
 #Exercise 2 — LEFT JOIN + COALESCE
 #List all clients with their total credit limit. If a client has no credit assessment, show 0 instead of NULL.
+
+select client_name, sum(credit_limit) as total_credit_limit 
+from clients cli
+left join credit_assessments cre
+on cli.client_id = cre.client_id
+where status != 'Rejected'
+group by client_name;
+
 
 #Exercise 3 — CASE WHEN
 #For each claim, add a column called urgency that says 'High' if claim_amount > 30000, and 'Normal' otherwise.
 
+select *, 
+case
+when claim_amount > 30000 then 'High' else 'Normal' 
+end as urgency
+from claims;
+
 #Exercise 4 — WHERE vs HAVING
 #Count the number of Approved assessments per country. Only show countries where that count is at least 1.
+
+select country, 
+sum(case
+when status = 'Approved' then 1 else 0 end) as approved_assessments
+from credit_assessments cre
+join clients cli
+on cre.client_id = cli.client_id
+group by country
+having approved_assessments >=1 ;
