9a0887f2666666aff9919db62400112dc2b810e3
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-03-09 16:33:53 +0100

    Practice queries

--- week-02-filters/more_practice.sql
+++ week-02-filters/more_practice.sql
@@ -74,9 +74,12 @@ select pol.policy_id, cus.customer_name
 from policies pol
 left join customers cus on pol.customer_id = cus.customer_id;
 #correction: every policy must have a customer
-select pol.policy_id, cus.customer_name
-from policies pol
-join customers cus on pol.customer_id = cus.customer_id;
+SELECT 
+    pol.policy_id,
+    cus.customer_name
+FROM policies pol
+JOIN customers cus
+    ON pol.customer_id = cus.customer_id;
 #6 List all claims with their policy type.
 select cla.claim_id, pol.policy_type
 from claims cla
@@ -103,10 +106,14 @@ from customers cus
 join policies pol on cus.customer_id = pol.customer_id
 group by cus.customer_name;
 #correction: group by id, safer
-select cus.customer_id, cus.customer_name, sum(pol.premium) as total_premiun
-from customers cus
-join policies pol on cus.customer_id = pol.customer_id
-group by cus.customer_id,cus.customer_name;
+SELECT 
+    cus.customer_id,
+    cus.customer_name,
+    SUM(pol.premium) AS total_premium
+FROM customers cus
+JOIN policies pol
+    ON cus.customer_id = pol.customer_id
+GROUP BY cus.customer_id, cus.customer_name;
 #10 Count the number of policies per customer.
 select cus.customer_name, count(pol.policy_id) as total_policies
 from customers cus
@@ -131,11 +138,15 @@ join claims cla on pol.policy_id = cla.policy_id
 join customers cus on cus.customer_id = pol.customer_id
 group by pol.policy_id, cus.customer_name;
 #correction: remove the policy_id, it is just per customer
-select cus.customer_name,sum(cla.claim_amount) as total_claims_amount
-from policies pol 
-join claims cla on pol.policy_id = cla.policy_id
-join customers cus on cus.customer_id = pol.customer_id
-group by cus.customer_name;
+SELECT 
+    cus.customer_name,
+    SUM(cla.claim_amount) AS total_claim_amount
+FROM customers cus
+JOIN policies pol 
+    ON cus.customer_id = pol.customer_id
+JOIN claims cla 
+    ON pol.policy_id = cla.policy_id
+GROUP BY cus.customer_name;
 #14 Find the average claim amount.
 select avg(claim_amount) as avg_client_amount
 from claims;
@@ -168,12 +179,11 @@ join policies pol on cus.customer_id = pol.customer_id
 group by cus.customer_name
 having count(pol.customer_id) >1;
 #18 Find policies with no claims.
-select * from policies;
-select * from claims;
-select pol.policy_id, cla.claim_id
-from policies pol
-join claims cla on pol.policy_id = cla.policy_id
-where pol.policy_id is null;
+SELECT pol.policy_id
+FROM policies pol
+LEFT JOIN claims cla
+    ON pol.policy_id = cla.policy_id
+WHERE cla.policy_id IS NULL;
 #correction: wrong impossible condition. It says no claims = without claims, left join
 SELECT pol.policy_id
 FROM policies pol
@@ -190,6 +200,18 @@ join policies pol on cus.customer_id = pol.customer_id
 join claims cla on cla.policy_id = pol.policy_id
 ) as query_highest
 where rn = 1;
+#simple version 
+SELECT 
+    cus.country,
+    SUM(cla.claim_amount) AS total_claim_amount
+FROM customers cus
+JOIN policies pol
+    ON cus.customer_id = pol.customer_id
+JOIN claims cla
+    ON pol.policy_id = cla.policy_id
+GROUP BY cus.country
+ORDER BY total_claim_amount DESC
+LIMIT 1;
 #20 Find the top 3 customers by total claim amount.
 select customer_name, claim_amount
 from(
@@ -199,28 +221,223 @@ from customers cus
 join policies pol on cus.customer_id = pol.customer_id 
 join claims cla on cla.policy_id = pol.policy_id) as query_rn
 where rn <= 3;
+#simple version:
+SELECT 
+    cus.customer_name,
+    SUM(cla.claim_amount) AS total_claim_amount
+FROM customers cus
+JOIN policies pol
+    ON cus.customer_id = pol.customer_id
+JOIN claims cla
+    ON pol.policy_id = cla.policy_id
+GROUP BY cus.customer_name
+ORDER BY total_claim_amount DESC
+LIMIT 3;
 
-#Realistic 1-hour SQL mock test
+/* ==============================
+SQL MOCK TEST (60 MINUTES)
+Dataset: customers, policies, claims
+============================== */
+USE insurance_analytics;
+/* Q1 */
+-- List all customers who signed up after '2023-04-01'.
 
-#Set a 60 minute timer.
+select * 
+from customers
+where signup_date > '2023-04-01';
 
-#Question 1
-#List all customers who signed up after 2023-04-01.
+/* Q2 */
+-- Show all policies with their customer name and country.
 
-#Question 2
-#Show all policies with their customer name and country.
+select pol.policy_id, cus.customer_name, cus.country
+from policies pol
+left join customers cus on pol.customer_id = cus.customer_id
+;
 
-#Question 3
-#Find the total number of policies per country.
+/* Q3 */
+-- Find the total number of policies per country.
 
-#Question 4
-#Find the total claim amount per policy.
-#Question 5
-#Find policies that have no claims.
-#Question 6
-#Find the customer with the highest total claim amount.
-#Question 7
-#Find the average claim amount per country.
-#Question 8
-#Find customers who have more than 1 claim.
+select cus.country, count(pol.policy_id) as total_number_of_policies
+from policies pol
+join customers cus on pol.customer_id = cus.customer_id
+group by cus.country;
 
+/* Q4 */
+-- Find the total claim amount per policy.
+
+select pol.policy_id, sum(cla.claim_amount) as total_claim_amount
+from policies pol 
+join claims cla on pol.policy_id = cla.policy_id
+group by pol.policy_id;
+
+/* Q5 */
+-- Find policies that have no claims.
+
+select pol.policy_id, cla.claim_id
+from policies pol 
+left join claims cla on pol.policy_id = cla.policy_id
+where cla.claim_id is null;
+
+/* Q6 */
+-- Find the customer with the highest total claim amount.
+
+select cus.customer_name, max(cla.claim_amount) as highest_total_claim_amount
+from customers cus 
+join policies pol on pol.customer_id = cus.customer_id
+join claims cla on pol.policy_id = cla.policy_id
+group by cus.customer_name
+order by max(cla.claim_amount) desc
+limit 1;
+
+/* Q7 */
+-- Find the average claim amount per country.
+
+select cus.country, avg(cla.claim_amount) as avg_claim_amount
+from customers cus
+join policies pol on pol.customer_id = cus.customer_id
+join claims cla on pol.policy_id= cla.policy_id
+group by cus.country;
+
+/* Q8 */
+-- Find customers who have more than 1 claim.
+
+select cus.customer_name, count(cla.claim_id) total_claim
+from customers cus
+join policies pol on pol.customer_id = pol.customer_id
+join claims cla on pol.policy_id = cla.policy_id
+group by cus.customer_name
+having count(cla.claim_id) > 1;
+
+
+/* ==============================
+INSURANCE ANALYTICS QUESTIONS
+============================== */
+
+/* Q9 */
+-- Calculate the loss ratio per policy.
+-- loss_ratio = total_claim_amount / premium
+
+select policy_id, (total_claim_amount / premium) as loss_ratio 
+from (
+select pol.policy_id, pol.premium,sum(cla.claim_amount) as total_claim_amount
+from policies pol 
+join claims cla on pol.policy_id = cla.policy_id 
+group by pol.policy_id) as calcu_claim;
+
+/* Q10 */
+-- Find customers whose total claim amount is greater than 5000.
+
+select cus.customer_name, sum(cla.claim_amount) as total_claim_amount
+from customers cus
+join policies pol on cus.customer_id = pol.customer_id
+join claims cla on pol.policy_id = cla.policy_id
+group by cus.customer_name 
+having sum(cla.claim_amount) > 5000;
+
+/* Q11 */
+-- Calculate the total claim amount per month.
+
+select date_format(claim_date, '%Y-%m') as months, claim_id, sum(claim_amount) as total_claim_amount
+from claims 
+group by date_format(claim_date, '%Y-%m') , claim_id
+order by date_format(claim_date, '%Y-%m') asc;
+ 
+/* Q12 */
+-- Calculate the claim approval rate per country.
+-- approval_rate = approved_claims / total_claims
+
+select country, (approved_claims/total_claims) as approval_rate
+from(
+select cus.country, sum(cla.claim_amount) as total_claims,
+sum(case when cla.status = 'Approved' then 1 else 0 end) as approved_claims
+from customers cus
+join policies pol on cus.customer_id = pol.customer_id
+join claims cla on cla.policy_id = pol.policy_id
+group by cus.country) as sub_app; 
+
+/* Q13 */
+-- Find the top 3 policies with the highest total claim amount.
+
+select pol.policy_id, sum(cla.claim_amount) as total_claim_amount
+from policies pol 
+join claims cla on pol.policy_id = cla.policy_id
+group by pol.policy_id 
+order by sum(cla.claim_amount) desc
+limit 3;
+
+/* ==============================
+ADVANCED DATA ANALYST QUESTIONS
+============================== */
+
+/* Q14 */
+-- Find customers who have multiple policies.
+
+select cus.customer_name, count(pol.customer_id) as total_policies
+from customers cus
+join policies pol on cus.customer_id = pol.customer_id
+group by cus.customer_name
+having count(pol.customer_id) >1;
+
+/* Q15 */
+-- Find policies where the total claim amount exceeds the premium.
+
+select policy_id, total_claim_amount, premium
+from(
+select pol.policy_id, pol.premium,sum(cla.claim_amount) as total_claim_amount
+from policies pol
+join claims cla on pol.policy_id = cla.policy_id
+group by pol.policy_id
+having sum(cla.claim_amount) >  pol.premium) as query;
+
+/* Q16 */
+-- Find the average claim amount per policy type.
+
+select pol.policy_type, avg(cla.claim_amount) as avg_claim_amount
+from policies pol 
+join claims cla on pol.policy_id = cla.policy_id
+group by pol.policy_type; 
+
+/* Q17 */
+-- Find the country with the highest total premium.
+
+select* from policies;
+select cus.country, sum(pol.premium) as highest_total_premium
+from customers cus
+join policies pol on cus.customer_id = pol.customer_id
+group by cus.country
+order by sum(pol.premium) desc
+limit 1;
+
+/* Q18 */
+-- Find the percentage of rejected claims.
+
+select sum(case when status = 'Rejected' then 1 else 0 end)/count(status) percentage_of_rejected_claims
+from claims;
+
+/* Q19 */
+-- Find customers who have policies but no claims.
+
+select cus.customer_name
+from customers cus
+left join policies pol on cus.customer_id = pol.customer_id
+left join claims cla on pol.policy_id = cla.policy_id
+where cla.policy_id is null; 
+
+/* Q20 */
+-- Rank customers by their total claim amount (highest first).
+
+select customer_name, total_claim_amount, 
+rank()over(order by total_claim_amount desc) as rkn
+from(
+select cus.customer_name, sum(cla.claim_amount) as total_claim_amount
+from customers cus
+join policies pol on cus.customer_id = pol.customer_id
+join claims cla on pol.policy_id = cla.policy_id
+group by cus.customer_name) as total;
+
+/* Q21 */
+-- Find the cumulative claim amount ordered by claim date.
+
+select claim_id, claim_date,
+sum(claim_amount) over( order by claim_date asc) as cumulative_claim_amount
+from claims;
