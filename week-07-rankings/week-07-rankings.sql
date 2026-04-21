f7e2db05ff8d33b8199f11b397474d591a03beb0
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-04-21 10:56:58 +0200

    Week 7: SQL Rankings Practice

---
+++ week-07-rankings/week-07-rankings.sql
@@ -0,0 +1,16 @@
+# Week 7 : Rankings
+
+#Load schema
+USE superstore;
+
+#Exercise 1: Row Number and Rank
+select customer_name,
+sum(sales) as total_sales,
+row_number()over(order by sum(sales) desc) as row_num,
+rank()over(order by sum(sales) desc) as rnk_gaps,
+dense_rank()over(order by sum(sales) desc) as rnk_nogaps
+from sales_raw
+group by customer_name
+order by total_sales desc
+limit 10;
+
