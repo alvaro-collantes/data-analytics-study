202a9c41471be1beb095286f86c6d57f5d3369f5
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-04-26 23:22:36 +0200

    Exercise 1

--- week-08-logic/week-08-logic.sql
+++ week-08-logic/week-08-logic.sql
@@ -3,3 +3,16 @@
 #Load schema
 USE superstore;
 
+#Exercise 1: Rank of the monthly sales
+With monthly_sales as (
+select date_format(order_date,'%Y-%m') as ym, sum(sales) as total_sales
+from sales_raw
+group by ym), 
+ranked_months as (
+select ym, total_sales,
+rank() over(order by total_sales desc) as rnk
+from monthly_sales)
+select * 
+from ranked_months
+where rnk <= 5
+;
