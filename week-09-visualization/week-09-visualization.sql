9f10957ad5b65bb03e724f1c57a95a225b0f48e3
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-05-01 16:50:01 +0200

    Exercise 1

--- week-09-visualization/week-09-visualization.sql
+++ week-09-visualization/week-09-visualization.sql
@@ -3,3 +3,16 @@
 #Load schema
 USE superstore;
 
+#Exercise 1: Monthly Sales Trend
+Create view monthly_sales as 
+Select 
+	date_format(order_date,'%Y-%m') as year_months,
+    sum(sales) as total_sales,
+    sum(profit) as total_profit,
+    count(distinct(order_id)) as num_orders
+from sales_raw
+group by year_months;
+
+select * 
+from monthly_sales 
+where year_months >= '2017';
