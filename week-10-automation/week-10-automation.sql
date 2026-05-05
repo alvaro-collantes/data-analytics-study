63492f13d4bc34f08303dac20a947aa37fadf4ff
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-05-05 15:54:02 +0200

    Exercise 1

--- week-10-automation/week-10-automation.sql
+++ week-10-automation/week-10-automation.sql
@@ -3,3 +3,11 @@
 #Load schema
 USE superstore;
 
+#Exercise 1: Create Index (For West Region)
+create index idx_region on sales_raw(region); 
+create index idx_category on sales_raw(category);
+create index idx_order_date on sales_raw(order_date);
+#Check indexes
+Show index from sales_raw;
+#Verify the query uses the index
+EXPLAIN SELECT * FROM sales_raw WHERE Region = 'West';
