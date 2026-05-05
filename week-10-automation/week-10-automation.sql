2981e13240ad37d3e8281ce14e508ab53f539be7
Alvaro Collantes <alvarogabrielch@gmail.com>
2026-05-05 16:00:06 +0200

    Exercise 2

--- week-10-automation/week-10-automation.sql
+++ week-10-automation/week-10-automation.sql
@@ -11,3 +11,20 @@ create index idx_order_date on sales_raw(order_date);
 Show index from sales_raw;
 #Verify the query uses the index
 EXPLAIN SELECT * FROM sales_raw WHERE Region = 'West';
+
+#Exercise 2: Create the view 
+CREATE VIEW v_sales_detail AS
+SELECT
+    YEAR(order_date)            AS year,
+    MONTH(order_date)           AS month,
+    DATE_FORMAT(order_date, '%Y-%m') AS year_months,
+    Category, Region, Segment,
+    SUM(sales)                  AS total_sales,
+    SUM(profit)                 AS total_profit,
+    COUNT(*)                    AS num_rows
+FROM sales_raw
+GROUP BY year, month, year_months, Category, Region, Segment;
+
+#Filter Query
+SELECT * FROM v_sales_detail 
+WHERE year = 2017 AND Region = 'West';
